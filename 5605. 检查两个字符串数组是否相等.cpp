5958a0f06659fc8f64235c310729585e4a8d0986
hzh0 <2505333024@qq.com>
2020-11-22 12:06:32 +0800

    2020-11-22

---
+++ 5605. 检查两个字符串数组是否相等.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        string str1;
+        string str2;
+        for (auto i : word1) str1 += i;
+        for (auto i : word2) str2 += i;
+        return str1 == str2;
+    }
+};
