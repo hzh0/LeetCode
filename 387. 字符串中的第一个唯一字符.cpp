6dc38834f590dd01fce34ea7963e4452b616e2c2
hzh0 <2505333024@qq.com>
2020-12-23 16:35:15 +0800

    2020-12-23

--- 387. 字符串中的第一个唯一字符.cpp
+++ 387. 字符串中的第一个唯一字符.cpp
@@ -8,3 +8,18 @@ public:
         return -1;
     }
 };
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        if (s.empty()) return -1;
+        int arr[26]{0};
+        for (auto& i:s) {
+            arr[i-'a']++;
+        }
+        for (auto i = 0; i < s.size(); ++i) {
+            if (arr[s[i]-'a'] == 1) return i;
+        }
+        return -1;
+    }
+};
